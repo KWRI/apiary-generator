f13ac6f5417a3ffe47042b7ce9d3030b7f73b98e
olegfedoliak <oleg.fedoliak@gmail.com>
2017-06-28 11:31:47 +0300

    Added template

--- src/Console/ApiaryCommand.php
+++ src/Console/ApiaryCommand.php
@@ -3,6 +3,8 @@
 namespace KWRI\ApiaryGenerator\Console;
 
 use Illuminate\Console\Command;
+use KWRI\ApiaryGenerator\Generators\AbstractParser;
+use KWRI\ApiaryGenerator\Generators\RouteParser;
 
 class ApiaryCommand extends Command
 {
@@ -23,23 +25,35 @@ class ApiaryCommand extends Command
     protected $signature = 'apiary:generate 
                              {--route= : The router to be used}
                              {--user= : The user ID to use for API response calls}
+                             {--bindings= : Bindings}
                         ';
+
+    protected $db;
 
 
     public function __construct()
     {
         parent::__construct();
+        $this->db = app()->make('db');
 
     }
 
 
     public function handle()
     {
+
         $this->setUserToBeImpersonated($this->option('user'));
+        $route = $this->option('route');
+
+
+        $routeParser = new RouteParser();
+        $parsedRoutes = $this->processLaravelRoutes($routeParser, $route);
 
     }
 
     
+
+
 
 
 
@@ -57,4 +71,102 @@ class ApiaryCommand extends Command
         }
     }
 
+
+    private function processLaravelRoutes(AbstractParser $generator, $routePrefix)
+    {
+        $routes = $this->getRoutes();
+        $bindings = $this->getBindings();
+
+        $parsedRoutes = [];
+        foreach ($routes as $route) {
+            //$this->info($route->getUri());
+
+            try {
+                $this->db->beginTransaction();
+
+                if (str_contains($route->getUri(), $routePrefix)) {
+
+                    $this->info($route->getUri());
+
+                    if ($parsedRoute = $this->setRouteResponse($route, $generator, $bindings)) {
+                        dd($parsedRoutes);
+                        $parsedRoutes[] = $parsedRoute;
+                    }
+                }
+
+                $this->db->rollBack();
+            } catch (\Exception $e) {
+                $this->error($e->getMessage());
+            }
+        }
+
+        return $parsedRoutes;
+    }
+
+    private function getRoutes()
+    {
+        return \Route::getRoutes();
+    }
+
+    private function getBindings()
+    {
+        $bindings = $this->option('bindings');
+        if (empty($bindings)) {
+            return [];
+        }
+        $bindings = explode('|', $bindings);
+        $resultBindings = [];
+        foreach ($bindings as $binding) {
+            list($name, $id) = explode(',', $binding);
+            $resultBindings[$name] = $id;
+        }
+
+        return $resultBindings;
+    }
+
+
+    private function setRouteResponse($route, $generator, $bindings)
+    {
+        $generator->processRoute($route, $bindings);
+    }
+
+
+    /**
+     * @param $fileContent
+     * @return mixed
+     */
+    public function prepare($fileContent)
+    {
+        $replacings = [
+            '{name}',
+            '{namespace}',
+            '{testNamespace}',
+            '{table}',
+            '{getters}',
+            '{interfaceGetters}',
+            '{fillable}',
+            '{repository}',
+            '{directory}',
+            '{eloquentTest}',
+            '{repositoryTest}',
+            '{repositoryTestData}',
+        ];
+
+        $replacements = [
+            $this->getProvidedName(),
+            $this->getAppNamespace(),
+            $this->getTestsNamespace(),
+            $this->getTable(),
+            $this->getSettersGetters($this->fields),
+            $this->getSettersGetters($this->fields, true),
+            $this->getFillable($this->fields),
+            $this->getRepositoryPayloads($this->fields),
+            $this->getDirectory(),
+            $this->getEloquentTests($this->fields),
+            $this->getRepositoryTests($this->fields),
+            $this->getRepositoryDataTests($this->fields),
+        ];
+
+        return str_replace($replacings, $replacements, $fileContent);
+    }
 }
---
+++ src/Generators/RouteParser.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace KWRI\ApiaryGenerator\Generators;
+
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Request;
+use KWRI\ApiaryGenerator\Generators\AbstractParser;
+
+/**
+ * Class RouteParser.
+ */
+class RouteParser extends AbstractParser
+{
+    /**
+     * @param Route $route
+     *
+     * @return mixed
+     */
+    protected function getUri($route)
+    {
+        return $route->getUri();
+    }
+
+    /**
+     * @param \Illuminate\Routing\Route $route
+     * @param array                     $bindings
+     * @param array                     $headers
+     * @param bool                      $withResponse
+     *
+     * @return array
+     */
+    public function processRoute($route, $bindings = [], $headers = [], $withResponse = true)
+    {
+
+        $routeAction = $route->getAction();
+        $routeGroup = $this->getRouteGroup($routeAction['uses']);
+        $routeDescription = $this->getRouteDescription($routeAction['uses']);
+
+        $parsedRoute = $this->getParameters([
+            'id' => md5($route->getUri().':'.implode($route->getMethods())),
+            'resource' => $routeGroup,
+            'title' => $routeDescription['short'],
+            'description' => $routeDescription['long'],
+            'methods' => $route->getMethods(),
+            'uri' => $route->getUri(),
+            'parameters' => [],
+            'response' => '',
+            'responseCode' => '',
+        ], $routeAction, $bindings);
+
+        if ($withResponse) {
+            $response = $this->getRouteResponse($route, $bindings, $headers, $parsedRoute);
+            if ($response->headers->get('Content-Type') === 'application/json' || !empty($response)) {
+                $content = json_encode(json_decode($response->getContent()), JSON_PRETTY_PRINT);
+            } else {
+                $content = $response->getContent();
+            }
+
+            if ($response->getStatusCode() < 301) {
+                $parsedRoute['response'] = $content;
+                $parsedRoute['responseCode'] = $response->getStatusCode();
+            }
+        }
+
+        return $parsedRoute;
+    }
+
+    /**
+     * Call the given URI and return the Response.
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array  $parameters
+     * @param array  $cookies
+     * @param array  $files
+     * @param array  $server
+     * @param string $content
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function callRoute($method, $uri, $parameters = [],
+                              $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $kernel = App::make('Illuminate\Contracts\Http\Kernel');
+        App::instance('middleware.disable', true);
+
+        $server = collect([
+            //'CONTENT_TYPE' => 'application/json',
+            //'Accept' => 'application/json',
+        ])->merge($server)->toArray();
+
+        $request = Request::create(
+            $uri, $method, $parameters,
+            $cookies, $files, $this->transformHeadersToServerVars($server), $content
+        );
+
+        //if(!empty($parameters)) dd($request);
+        $response = $kernel->handle($request);
+
+        //if(!empty($parameters)) dd($response);
+
+        $kernel->terminate($request, $response);
+
+        return $response;
+    }
+}
