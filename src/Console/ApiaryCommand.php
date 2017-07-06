380d7d4122b011635ad55530671b13919a27c982
olegfedoliak <oleg.fedoliak@gmail.com>
2017-07-06 13:10:47 +0300

    bread

--- src/Console/ApiaryCommand.php
+++ src/Console/ApiaryCommand.php
@@ -213,6 +213,7 @@ class ApiaryCommand extends Command
             '{resource}',
             '{url}',
             '{token}',
+            '{includes}',
         ];
 
         $replacements = [
@@ -223,6 +224,7 @@ class ApiaryCommand extends Command
             $this->resource,
             $this->url,
             $this->user->getAccessToken()->getAccessToken(),
+            $this->includes,
         ];
 
         return str_replace($replacings, $replacements, $fileContent);
